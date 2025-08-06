28129f8d7e532c1241831695174947a1c3819014
Fernando Soares <116040735+fernando-ss-soares@users.noreply.github.com>
2025-08-05 23:30:10 -0300

    Update banner link and add metadata to _document.tsx
    The banner now links to the project's GitHub repository and opens in a new tab. Added metadata including title, description, creator, keywords, and Open Graph data to _document.tsx for improved SEO and social sharing.

--- components/banner/index.tsx
+++ components/banner/index.tsx
@@ -6,10 +6,11 @@ export default function BannerComponent() {
         tabIndex={-1}
         className="flex gap-8 justify-center items-start py-3 px-4 w-full bg-black border border-b border-black sm:items-center lg:py-4"
       >
-        <p className="text-sm font-light text-white">
+        <p className="text-sm font-light text-center text-white">
           <a
             className="font-medium underline text-primary-600 hover:no-underline"
-            href="#"
+            href="https://github.com/fernando-ss-soares/Front-End-RemedioJa"
+            target="_blank"
           >
             RemédioJá:
           </a>{" "}
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,28 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'RemédioJá',
+  description: 'O melhor site de acesso a médicamentos baratos do Brasil',
+  creator: "Fernando Soares",
+  keywords: ["Brasil", "Medicamentos", "RémedioJá", "Remédio barato"],
+  openGraph: {
+    title: 'RemédioJá',
+    description: 'O melhor site de acesso a médicamentos baratos do Brasil',
+    url: 'https://front-end-preco-medicamentos.vercel.app',
+    siteName: 'RemédioJá',
+    images: [
+      {
+        url: 'https://front-end-preco-medicamentos.vercel.app/drug.svg', // Deve ser uma URL absoluta
+        width: 1200,
+        height: 630,
+        alt: 'Imagem de destaque da Página Inicial',
+      },
+    ],
+    locale: 'pt_BR',
+    type: 'website',
+  },
+};
 
 export default function Document() {
   return (
